5c00b81ca4fd7d778a5ac19347e801b2cc9ad1dd
Delusoire <deluso7re@outlook.com>
2024-06-12 22:25:07 +0100

    feat: use userAgent to infer spotify version

--- static.ts
+++ static.ts
@@ -3,8 +3,5 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import { fetchText } from "./util.js";
-
-export const SPOTIFY_VERSION = await fetchText("/xpui.js").then(
-	(xpui) => xpui.match(/clientVersion:"(?<version>\d+\.\d+\.\d+)/)!.groups!.version,
-);
+const { version } = window.navigator.userAgent.match(/Spotify\/(?<version>.+)\s/)!.groups!;
+export const SPOTIFY_VERSION = version.slice(0, version.lastIndexOf("."));
