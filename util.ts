c442505a8ca335660b73e49c6c6ae5d023719429
Delusoire <deluso7re@outlook.com>
2024-08-23 12:40:03 +0100

    y

--- util.ts
+++ util.ts
@@ -25,7 +25,7 @@ export function findBy(...tests: Array<string | RegExp | Predicate<any>>) {
 export const fetchText = (path: string) =>
 	fetch(path).then((res) => res.text()).catch(() => null);
 export const fetchJson = <R>(path: string) =>
-	fetch(path).then((res) => res.json()).catch(() => null) as Promise<R>;
+	fetch(path).then((res) => res.json()).catch(() => null) as Promise<Awaited<R>>;
 
 // assumption: str[start] === pair[0]
 export const findMatchingPos = (
